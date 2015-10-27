e4218c183febd496c008703d4d7939324ff3e162
lios87 <eliodeb@gmail.com>
2015-10-27 18:41:11 +0000

    Playing with Triangle In Unity

--- Assets/TheGame.cs
+++ Assets/TheGame.cs
@@ -2,18 +2,21 @@
 using System.Collections;
 
 public class TheGame : MonoBehaviour {
-	static readonly int[] triangle_indices = { 0, 1, 2 };
-	static readonly int[] line_indices = { 0, 1, 1, 2, 2, 0 };
+	static readonly int[] triangle_indices = { 0, 1, 2,2,3,0 };
+	static readonly int[] line_indices = { 0, 1, 1, 2, 2,3,3, 0 };
 	static readonly Vector3[] vertices = {
-		new Vector3(-10, -10, 0),
-		new Vector3(  0,  10, 0),
-		new Vector3( 10, -10, 0),
+		new Vector3(-5, -5, 0),
+		new Vector3(-5, 5, 0),
+		new Vector3( 5, 5, 0),
+		new Vector3( 5,-5, 0),
 	};
 
-	void make_triangle() {
+	void make_triangle(Vector3 pos) {
 		GameObject go = new GameObject ();
-		go.name = "triangle";
-		go.transform.localPosition = new Vector3 (-10, 0, 20);
+		go.name = "Square";
+		
+		go.transform.localPosition = new Vector3 (-15, 0, 20);
+		go.transform.Translate(new Vector3(pos.x, pos.y, pos.z));
 		
 		MeshFilter mf = go.AddComponent<MeshFilter> ();
 		go.AddComponent<MeshRenderer> ();
@@ -23,6 +26,10 @@ public class TheGame : MonoBehaviour {
 		mesh.triangles = triangle_indices;
 		mf.mesh = mesh;
 	}
+	
+	
+	
+	
 	
 	void make_wireframe() {
 		GameObject go = new GameObject ();
@@ -40,8 +47,15 @@ public class TheGame : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		make_triangle ();
-		make_wireframe ();
+		int i;
+		for (i=0;i<3;i++)
+		{
+			Vector3 temp = new Vector3(15*i, -5 + i*10, i*10);
+			make_triangle (temp);
+		
+		}
+		
+		//make_wireframe ();
 	}
 	
 	// Update is called once per frame
